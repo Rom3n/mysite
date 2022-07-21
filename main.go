adfb88e3c1d37a3a9bf087e5ded64f39dfbc0b29
Barbiturate <romaenot1997@gmail.com>
2022-07-22 01:51:36 +0300

    add HTML/CSS

--- main.go
+++ main.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u *User) getAllInfo() string {
+	return fmt.Sprintf("User name is %s. He is %d years old with %d bucks", u.Name, u.Age, u.Money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Initial commit")
+
+	bob := User{"Bob", 25, 50, 4.2, 0.8, []string{"eblya", "greblya", "rybalka"}}
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+	//bob := User{"Bob", 25, 50, 4.2, 0.8}
+	//bob.setNewName("Not Bob")
+	//fmt.Fprintf(w, bob.getAllInfo())
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Contacts")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts", contacts_page)
+	http.ListenAndServe("localhost:8080", nil)
 }
 
 func main() {
-	http.HandleFunc("/", home_page)
-	http.ListenAndServe("localhost:8080", nil)
+	handleRequest()
 }
